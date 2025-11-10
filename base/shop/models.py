f0dde2ca1c2c16006fe1f789321b8423e4302787
Luriando <196345190+Luriand@users.noreply.github.com>
2025-11-10 18:54:31 +0100

    Update shop models and database
    - Modified db.sqlite3 to incorporate recent changes.
    - Updated settings.pyc and views.pyc in the cache.
    - Added Category model in shop/models.py with title and created_at fields.

--- base/shop/models.py
+++ base/shop/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 # this is for data base.
+
+# models are created through classes
+
+class Category(models.Model):
+    title = models.CharField(max_length=255) # this is a field for the category title
+    created_at = models.DateTimeField(default=timezone.now)
