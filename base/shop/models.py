26a513a38ab27434bb68b9430d4ef90feb007d30
Luriando <196345190+Luriand@users.noreply.github.com>
2025-11-12 01:42:19 +0100

    Add Course model to shop app and update INFO_TUTORIAL.txt

--- base/shop/models.py
+++ base/shop/models.py
@@ -10,3 +10,12 @@ from django.utils import timezone
 class Category(models.Model):
     title = models.CharField(max_length=255) # this is a field for the category title
     created_at = models.DateTimeField(default=timezone.now)
+
+
+# --- Course model ---
+class Course(models.Model):
+    title = models.CharField(max_length = 255) 
+    price = models.FloatField() # float number 
+    students_qty = models.IntengerField() # amount of students
+    review_qty = models.IntengerField() # int for a score
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # when we will delete this category, everything will be deleted from this category
